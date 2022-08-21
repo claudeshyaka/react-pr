3d50be8033e52aa1bf47936650fdd48285c86675
claudeshyaka <elieclaude23@gmail.com>
2022-08-21 10:12:42 +0200

    removed jsx project

---
+++ jsx/src/index.js
@@ -0,0 +1,21 @@
+// 1) import react and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// 2) Get a reference to the div with ID root 
+const el = document.getElementById('root');
+
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a component
+function App() {
+    return (
+        <input 
+            placeholder="Hi there"
+        />
+    );
+};
+ 
+// 5) Show the component on the screen
+root.render(<App/>);
