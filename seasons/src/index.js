31db23ad8245a24aa711544a461d0820ab78ba7d
claudeshyaka <elieclaude23@gmail.com>
2022-12-25 12:24:30 -0500

    Clock, SeasonDisplay, Spinner,

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,10 @@
+import './style/App.css';
 // 1) import react and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
 
 // 2) Get a reference to the div with ID root 
 const el = document.getElementById('root');
@@ -10,14 +14,32 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 class App extends React.Component {
-    render() {
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
-    
+    }
+
+    renderContent () {
+        if (this.state.errorMessage && !this.setState.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+
+        return <Spinner  message="Please accept location request"/>
+    }
+
+    render() {
         return (
-            <div>Hi there!</div>
+            <div>
+                {this.renderContent()}
+            </div>
         );
     };
 };
